9f3dc7bf4f3cb47ae7a128a420abf3afc6c296f8
Satoshi MIMURA <mimura1133@hotmail.co.jp>
2016-01-26 07:26:27 +0900

    適当な色づけ機能搭載。

--- Src/TeXClassifier.cs
+++ Src/TeXClassifier.cs
@@ -1,53 +1,144 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Classification;
-using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
 
 namespace VsTeXProject
 {
-    class TeXClassifier : IClassifier
+    [Export(typeof(IClassifierProvider))]
+    [ContentType("vstex")]
+    internal class TeXClassifierProvider : IClassifierProvider
     {
-        private IClassificationType _classificationType;
-        private ITagAggregator<TeXTag> _tagger;
+        /// <summary>
+        ///     Import the classification registry to be used for getting a reference
+        ///     to the custom classification type later.
+        /// </summary>
+        [Import] internal IClassificationTypeRegistryService ClassificationRegistry = null;
 
-        internal TeXClassifier(ITagAggregator<TeXTag> tagger, IClassificationType todoType)
+        public TeXClassifierProvider()
         {
-            _tagger = tagger;
-            _classificationType = todoType;
+        }
+
+        public IClassifier GetClassifier(ITextBuffer buffer)
+        {
+            return
+                buffer.Properties.GetOrCreateSingletonProperty(
+                    delegate { return new TeXClassifier(ClassificationRegistry); });
+        }
+    }
+
+    internal class TeXClassifier : IClassifier
+    {
+        private readonly IClassificationType _normalType;
+        private readonly IClassificationType _commentOutType;
+        private readonly IClassificationType _beginEndType;
+        private readonly IClassificationType _functionType;
+        private readonly IClassificationType _braceType;
+
+        internal TeXClassifier(IClassificationTypeRegistryService registry)
+        {
+            _normalType = registry.GetClassificationType("TeXClassifierNormalFormat");
+            _commentOutType = registry.GetClassificationType("TeXClassifierCommentOutFormat");
+            _beginEndType = registry.GetClassificationType("TeXClassifierBeginEndFormat");
+            _functionType = registry.GetClassificationType("TeXClassifierFunctionFormat");
+            _braceType = registry.GetClassificationType("TeXClassifierBraceFormat");
         }
 
         /// <summary>
-        /// Get every ToDoTag instance within the given span. Generally, the span in 
-        /// question is the displayed portion of the file currently open in the Editor
+        ///     This method scans the given SnapshotSpan for potential matches for this classification.
+        ///     In this instance, it classifies everything and returns each span as a new ClassificationSpan.
         /// </summary>
-        /// <param name="span">The span of text that will be searched for ToDo tags</param>
-        /// <returns>A list of every relevant tag in the given span</returns>
+        /// <param name="trackingSpan">The span currently being classified</param>
+        /// <returns>A list of ClassificationSpans that represent spans identified to be of this classification</returns>
         public IList<ClassificationSpan> GetClassificationSpans(SnapshotSpan span)
         {
-            IList<ClassificationSpan> classifiedSpans = new List<ClassificationSpan>();
+            var classifications = new List<ClassificationSpan>();
+            var text = span.GetText();
 
-            var tags = _tagger.GetTags(span);
-
-            foreach (IMappingTagSpan<TeXTag> tagSpan in tags)
+            if (text == "")
             {
-                SnapshotSpan todoSpan = tagSpan.Span.GetSpans(span.Snapshot).First();
-                classifiedSpans.Add(new ClassificationSpan(todoSpan, _classificationType));
+                return classifications;
             }
 
-            return classifiedSpans;
-        }
+            for (int pt = span.Start; pt < span.End; pt++)
+            {
 
-        /// <summary>
-        /// Create an event for when the Classification changes
-        /// </summary>
-        public event EventHandler<ClassificationChangedEventArgs> ClassificationChanged
-        {
-            add { }
-            remove { }
+                if (text == "")
+                    return classifications;
+
+                if (text[0] == '%')
+                {
+                    classifications.Add(
+                        new ClassificationSpan(new SnapshotSpan(span.Snapshot, new Span(pt, text.Length)),
+                            this._commentOutType));
+                    return classifications;
+                }
+
+                if (text[0] == '\\')
+                {
+                    var color = this._functionType;
+                    var len = 0;
+                    var paramnest = 0;
+                    var maxlines = 10;
+
+                    if (text.StartsWith("\\begin") || text.StartsWith("\\end"))
+                    {
+                        color = this._beginEndType;
+                    }
+
+                    for (len = 1; len < text.Length; len++)
+                    {
+                        if (text[len] == '{' || text[len] == '[' || text[len] == '}' || text[len] == ']' ||
+                            text[len] == ' ')
+                            break;
+                    }
+
+                    var snaptext = text;
+                    var line = span.Snapshot.GetLineNumberFromPosition(pt);
+                    var start = pt;
+                    var snapshotline = span.Snapshot.GetLineFromLineNumber(line);
+
+                    do
+                    {
+                        
+                        for (; len < snaptext.Length; len++)
+                        {
+                            if (snaptext[len] == '{' || snaptext[len] == '[') paramnest++;
+                            if (snaptext[len] == '}' || snaptext[len] == ']') paramnest--;
+                            if (snaptext[len] == '\\') len++;
+
+                            if (paramnest == 0 && snaptext[len] != ' ' && snaptext[len] != '{' && snaptext[len] != '[' &&
+                                snaptext[len] != '}' && snaptext[len] != ']') break;
+                        }
+                        classifications.Add(new ClassificationSpan(new SnapshotSpan(snapshotline.Snapshot, start, len),color));
+
+                        maxlines--;
+                        if (maxlines <= 0) break;
+                        if (paramnest == 0) break;
+
+                        line++;
+                        if (line < span.Snapshot.Lines.Count())
+                        {
+                            snapshotline = span.Snapshot.GetLineFromLineNumber(line);
+                            snaptext = snapshotline.GetTextIncludingLineBreak();
+                            start = snapshotline.Start;
+                            len = 0;
+                        }
+                    } while (line < span.Snapshot.Lines.Count());
+                }
+
+                text = text.Substring(1);
+            }
+
+            return classifications;
         }
+#pragma warning disable 67
+        public event EventHandler<ClassificationChangedEventArgs> ClassificationChanged;
+#pragma warning restore 67
     }
 }
---
+++ Src/TeXContentTypeDefinitions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Utilities;
+
+namespace VsTeXProject
+{
+
+    internal static class TeXContentTypeDefinitions
+    {
+        [Export] [Name("vstex")] [BaseDefinition("text")] internal static ContentTypeDefinition
+            hidingContentTypeDefinition = null;
+
+        [Export] [FileExtension(".tex")] [ContentType("vstex")] internal static FileExtensionToContentTypeDefinition
+            hiddenFileExtensionDefinition = null;
+
+    }
+}
